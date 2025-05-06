c39122bf54f5c99fa914750ab7dea815301535ab
Salim Ramirez <salimramirezm@gmail.com>
2025-05-06 18:25:21 -0500

    feat(in-app-navigation): setup in-app navigation rules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/pages/dashboard/dashboard.component';
+import { SearchComponent } from './billing/pages/search/search.component';
 
 export const routes: Routes = [
-
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
