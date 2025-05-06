f47a5ac924919236f4983303f664000c5b1fa8a6
Salim Ramirez <salimramirezm@gmail.com>
2025-05-06 16:26:50 -0500

    feat(navigation): implement main layout bar

--- src/app/public/components/side-navigation-bar/side-navigation-bar.component.ts
+++ src/app/public/components/side-navigation-bar/side-navigation-bar.component.ts
@@ -1,13 +1,15 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatIcon } from "@angular/material/icon";
 import { MatAnchor, MatButton } from "@angular/material/button";
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-side-navigation-bar',
   imports: [
     MatIcon,
     MatButton,
-    MatAnchor
+    MatAnchor,
+    MatListModule
   ],
   templateUrl: './side-navigation-bar.component.html',
   styleUrl: './side-navigation-bar.component.css'
