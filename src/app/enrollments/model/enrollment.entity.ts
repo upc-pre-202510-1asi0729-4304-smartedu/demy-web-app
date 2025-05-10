b73fbde42db9f9aa5031a54947c48e64e78d53e0
Kyrubi <u20221c486@upc.edu.pe>
2025-05-09 20:29:13 -0500

    feat(enrollments): add enrollments entity.

--- src/app/enrollments/model/enrollment.entity.ts
+++ src/app/enrollments/model/enrollment.entity.ts
@@ -1,19 +1,37 @@
 import {Student} from './student.entity';
+import {AcademicPeriod} from './academic-period.entity';
 
 export class Enrollment {
   id?: string
+  student: Student
   studentId: string;
-  period: string;
+  periodId: string;
+  period: AcademicPeriod
+  amount: number;
   enrollmentStatus: EnrollmentStatus;
-  constructor(id = '', studentId = '', period = '', enrollmentStatus = EnrollmentStatus.ACTIVE) {
+  paymentStatus: PaymentStatus;
+  constructor(id = '', student = new Student(), studentId = '', periodId = '', period = new AcademicPeriod(), enrollmentStatus = EnrollmentStatus.ACTIVE, amount = 0, paymentStatus = PaymentStatus.PENDING) {
     this.id = id;
+    this.student = student;
     this.studentId = studentId;
+    this.periodId = periodId;
     this.period = period;
     this.enrollmentStatus = enrollmentStatus;
+    this.amount = amount;
+    this.paymentStatus = paymentStatus;
   }
 }
 
 export enum EnrollmentStatus {
   ACTIVE = 'ACTIVE',
   CANCELLED = 'CANCELLED',
+  COMPLETED = 'COMPLETED',
+  DELETED = 'DELETED'
+}
+
+export enum PaymentStatus {
+  PENDING = 'PENDING',
+  PAID = 'PAID',
+  REFUNDED = 'REFUNDED',
+  PARTIAL = 'PARTIAL'
 }
