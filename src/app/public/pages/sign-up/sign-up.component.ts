7f9ee7b04e61116a299c55efadd69863b37df2c9
Radv2005 <U202312318@upc.edu.pe>
2025-05-09 22:40:06 -0500

    feat(sign-up): finished sign-up component box

--- src/app/public/pages/sign-up/sign-up.component.ts
+++ src/app/public/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  signUpForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      remember: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log('Formulario enviado:', this.signUpForm.value);
+    }
+  }
 }
