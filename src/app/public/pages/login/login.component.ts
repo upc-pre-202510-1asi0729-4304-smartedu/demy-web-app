da2a62a5add18ba27d4a4aae908f38706d649234
Radv2005 <U202312318@upc.edu.pe>
2025-05-10 01:24:40 -0500

    feat(login): configure i18n for login box

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -6,21 +6,27 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageSwitcherComponent } from '../../../shared/components/language-switcher/language-switcher.component';
+
 
 
 @Component({
   selector: 'app-login',
   imports: [
+    LanguageSwitcherComponent,
     RouterModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    TranslateModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
   loginForm: FormGroup;
 
@@ -31,6 +37,7 @@ export class LoginComponent {
       remember: [false]
     });
   }
+
 
   onSubmit() {
     if (this.loginForm.valid) {
