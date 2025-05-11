fe622bb120252b9bf5301dc9cebed925bcb41c7c
DanielCr <u20221g120@upc.edu.pe>
2025-05-10 21:14:43 -0500

    feat(schedule): add schedule entity

---
+++ src/app/scheduling/model/schedule.entity.ts
@@ -0,0 +1,31 @@
+import { Course } from './course.entity';
+import { Classroom } from './classroom.entity';
+
+export interface TimeRange {
+  start: string;
+  end: string;
+}
+
+export class Schedule {
+  id: number;
+  dayOfWeek: string;
+  timeRange: TimeRange;
+  course: Course;
+  classroom: Classroom;
+  //teacher: Teacher; //Despues de agregar el profesor
+
+  constructor(schedule: {
+    id?: number,
+    dayOfWeek?: string,
+    timeRange?: TimeRange,
+    course?: Course,
+    classroom?: Classroom
+    //teacher?: Teacher  //Despues de agregar el profesor
+  }) {
+    this.id = schedule.id || 0;
+    this.dayOfWeek = schedule.dayOfWeek || '';
+    this.timeRange = schedule.timeRange || { start: '', end: '' };
+    this.course = schedule.course || new Course({});
+    this.classroom = schedule.classroom || new Classroom({});
+  }
+}
