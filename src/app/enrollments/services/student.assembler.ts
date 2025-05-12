0e8e702ebc20ce503091ac3d147b7de036fae9f7
Kyrubi <u20221c486@upc.edu.pe>
2025-05-12 10:01:12 -0500

    feat(students): add student assembler.

--- src/app/enrollments/services/student.assembler.ts
+++ src/app/enrollments/services/student.assembler.ts
@@ -1,2 +1,27 @@
-export class Student {
+import {Injectable} from '@angular/core';
+import {Student} from '../model/student.entity';
+import {StudentResource} from './students.response';
+import {Sex} from "../model/student.entity";
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StudentAssembler {
+
+  toEntityFromResource(studentResource: StudentResource): Student {
+    let sexEnum = studentResource.sex as Sex
+
+    return new Student(
+      studentResource.id,
+      studentResource.dni,
+      studentResource.first_name,
+      studentResource.last_name,
+      sexEnum,
+      new Date(studentResource.birth_date),
+      studentResource.address,
+      studentResource.phone_number);
+  }
+  toEntitiesFromResponse(response: StudentResource[]): Student[] {
+    return response.map(student => this.toEntityFromResource(student));
+  }
 }
