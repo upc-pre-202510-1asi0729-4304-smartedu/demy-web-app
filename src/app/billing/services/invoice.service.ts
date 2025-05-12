4ea9b01fab3d1392e27a21aa24b952fdb719a14d
Salim Ramirez <salimramirezm@gmail.com>
2025-05-11 21:02:00 -0500

    chore: remove map import

--- src/app/billing/services/invoice.service.ts
+++ src/app/billing/services/invoice.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BaseService } from '../../shared/services/base.service';
 import { Invoice } from '../model/invoice.entity';
 import { environment } from '../../../environments/environment';
-import {catchError, map, Observable, retry} from 'rxjs';
+import {catchError, Observable, retry} from 'rxjs';
 
 const invoicesResourceEndpointPath = environment.invoicesEndpointPath;
 
@@ -21,9 +21,6 @@ export class InvoiceService extends BaseService<Invoice> {
     return this.http.get<Invoice[]>(url, this.httpOptions).pipe(
       retry(2),
       catchError(this.handleError)
-      // json-server retorna un array, aunque haya una sola factura
-      // así que se toma la primera
-      //map(invoices => invoices[0])
     );
   }
 }
