ccf8035e58895dd1c5eb035f61045f0cd5d2a512
Kyrubi <u20221c486@upc.edu.pe>
2025-05-12 10:23:03 -0500

    feat(enrollments): add enrollment response.

---
+++ src/app/enrollments/services/enrollment.response.ts
@@ -0,0 +1,27 @@
+// services/resources/enrollment.resource.ts
+export interface EnrollmentsResponse {
+  enrollments: EnrollmentResource[];
+  total: number;
+  page: number;
+  limit: number;
+}
+
+export interface EnrollmentResourceRegistration {
+  student_id: string;
+  period_id: string;
+  enrollment_date: string; // ISO format
+  amount: number;
+  status: string;
+  payment_status: string;
+  date_created: string; // ISO format
+}
+export interface EnrollmentResource {
+  id: string
+  student_id: string;
+  period_id: string;
+  enrollment_date: string; // ISO format
+  amount: number;
+  status: string;
+  payment_status: string;
+  date_created: string; // ISO format
+}
