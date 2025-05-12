515d4ee5c629d0ee4ea17bc859c852e7bfd6b8fb
Kyrubi <u20221c486@upc.edu.pe>
2025-05-12 09:52:38 -0500

    feat(academic-periods): add academyId and isActive fields to entity

--- src/app/enrollments/model/academic-period.entity.ts
+++ src/app/enrollments/model/academic-period.entity.ts
@@ -1,12 +1,16 @@
 export class AcademicPeriod {
   id: string;
   name: string;
+  academyId: string
   startDate: Date;
   endDate: Date;
-  constructor(id = '', name = '', startDate = new Date(), endDate = new Date()) {
+  isActive: boolean;
+  constructor(id = '', name = '', academyId = '', startDate = new Date(), endDate = new Date(), isActive = true) {
     this.id = id;
     this.name = name;
+    this.academyId = academyId;
     this.startDate = startDate;
     this.endDate = endDate;
+    this.isActive = isActive
   }
 }
