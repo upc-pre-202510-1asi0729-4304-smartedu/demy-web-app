a9f3484866ec10bc7b9fb90d1b3556d6d5df26ba
Kyrubi <u20221c486@upc.edu.pe>
2025-05-12 10:04:19 -0500

    feat(students): add students response.

--- src/app/enrollments/services/students.response.ts
+++ src/app/enrollments/services/students.response.ts
@@ -1,2 +1,32 @@
-export interface Students {
+
+export interface StudentsResponse {
+    students: StudentResource[]
+}
+
+export interface StudentSummaryResource {
+    id: string;
+    dni: string;
+    first_name: string;
+    last_name: string;
+}
+
+export interface StudentResource {
+    id: string;
+    dni: string;
+    first_name: string;
+    last_name: string;
+    sex: 'MALE' | 'FEMALE';
+    birth_date: string;
+    address: string;
+    phone_number: string;
+}
+
+export interface StudentRegistrationResource {
+    dni: string;
+    first_name: string;
+    last_name: string;
+    sex: 'MALE' | 'FEMALE';
+    birth_date: string;
+    address: string;
+    phone_number: string;
 }
