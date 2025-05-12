ef22366718d9c82d2440f0c6f032dfa68b1fbe2f
Kyrubi <u20221c486@upc.edu.pe>
2025-05-12 09:59:38 -0500

    feat(environments): add enrollment and academic period endpoint paths.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: "https://6820406072e59f922ef8198b.mockapi.io/api/v1",
-  studentsEndpointPath: "/students"
+  studentsEndpointPath: "/students",
+  enrollmentEndpointPath: "/enrollments",
+  academicPeriodEndpointPath: "/academic-periods"
 };
