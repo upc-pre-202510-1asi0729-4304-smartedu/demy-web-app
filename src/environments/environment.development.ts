a245489eae7f44b7afb1eede0a14fef56a29f7f9
diegovilcasaboya <diegovilcatut@gmail.com>
2025-05-13 03:21:17 -0500

    feat: implement environment for attendance components

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,10 @@
 export const environment = {
+  // In real lifecycles, this file should not be part of version control
   production: false,
+  // Server Base URL for Fake REST API
+  // serverBaseURL: 'http://localhost:3000/api/v1'
+  // Server Base URL for Spring Boot REST API
+  // serverBaseURL: 'http://localhost:8080/api/v1'
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  coursesEndpointPath: '/courses',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  // In real lifecycles, this file should not be part of version control
   production: true,
+  // Server Base URL for REST API
+  serverBaseUrl: 'https://6820406072e59f922ef8198b.mockapi.io/api/v1',
+  coursesEndpointPath: '/class-sessions',
 };
