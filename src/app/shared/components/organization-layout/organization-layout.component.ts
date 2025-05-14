783d97b4b8bb146488c0da3c582a2c37b6c001d8
Salim Ramirez <salimramirezm@gmail.com>
2025-05-14 00:52:22 -0500

    feat: add weekly scheduling route

--- src/app/shared/components/organization-layout/organization-layout.component.ts
+++ src/app/shared/components/organization-layout/organization-layout.component.ts
@@ -12,16 +12,28 @@ import {OrganizationOptionComponent} from '../organization-option/organization-o
 export class OrganizationLayoutComponent {
   options = [
     {
-      title: 'organization.profile.title',
-      description: 'organization.profile.description',
-      image: 'assets/img/organization-profile.jpg',
-      route: '/organization/profile'
-    },
-    {
       title: 'organization.courses.title',
       description: 'organization.courses.description',
       image: 'assets/img/organization-courses.jpg',
       route: '/organization/courses'
+    },
+    {
+      title: 'organization.rooms.title',
+      description: 'organization.rooms.description',
+      image: 'assets/img/organization-rooms.jpg',
+      route: '/organization/classrooms'
+    },
+    {
+      title: 'organization.teachers.title',
+      description: 'organization.teachers.description',
+      image: 'assets/img/organization-teachers.jpg',
+      route: '/organization/teachers'
+    },
+    {
+      title: 'organization.weekly.title',
+      description: 'organization.weekly.description',
+      image: 'assets/img/organization-weekly.jpg',
+      route: '/organization/weekly-schedules'
     },
     {
       title: 'organization.periods.title',
@@ -29,17 +41,5 @@ export class OrganizationLayoutComponent {
       image: 'assets/img/organization-periods.jpg',
       route: '/organization/periods'
     },
-    {
-      title: 'organization.rooms.title',
-      description: 'organization.rooms.description',
-      image: 'assets/img/organization-rooms.jpg',
-      route: '/organization/rooms'
-    },
-    {
-      title: 'organization.teachers.title',
-      description: 'organization.teachers.description',
-      image: 'assets/img/organization-teachers.jpg',
-      route: '/organization/teachers'
-    }
   ];
 }
