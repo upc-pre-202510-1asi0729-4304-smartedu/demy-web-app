e845dc977d72b499aba9632363b4cacbb28b123d
Salim Ramirez <salimramirezm@gmail.com>
2025-06-17 16:19:48 -0500

    feat: update navOptions with roles

--- src/app/shared/components/side-navigation-bar/side-navigation-bar.component.ts
+++ src/app/shared/components/side-navigation-bar/side-navigation-bar.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatIcon } from "@angular/material/icon";
 import { MatListModule } from '@angular/material/list';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../../../iam-user/services/user.service';
 
 /**
  * Sidebar navigation component.
@@ -18,22 +19,42 @@ import { RouterLink } from '@angular/router';
   styleUrl: './side-navigation-bar.component.css'
 })
 export class SideNavigationBarComponent {
+  protected readonly userService = inject(UserService);
+  private readonly router = inject(Router);
   /**
    * List of navigation options with icon, label, and route.
    */
   navOptions = [
-    { icon: 'apartment', label: 'Mi organización', route: '/organization' },
-    { icon: 'assignment', label: 'Matrícula', route: '/enrollment' },
-    { icon: 'group', label: 'Alumnos', route: '/students' },
-    { icon: 'payments', label: 'Pagos', route: '/payments' },
-    { icon: 'schedule', label: 'Horarios', route: '/search-schedules' },
-    { icon: 'schedule', label: 'Mi Horario', route: '/my-schedule' },
-    { icon: 'check_circle', label: 'Asistencia', route: '/attendance' },
-    { icon: 'account_balance', label: 'Finanzas', route: '/finance' }
+    { icon: 'apartment', label: 'Mi organización', route: '/organization', roles: ['ADMIN'] },
+    { icon: 'assignment', label: 'Matrícula', route: '/enrollment', roles: ['ADMIN'] },
+    { icon: 'group', label: 'Alumnos', route: '/students', roles: ['ADMIN'] },
+    { icon: 'payments', label: 'Pagos', route: '/payments', roles: ['ADMIN'] },
+    { icon: 'schedule', label: 'Horarios', route: '/search-schedules', roles: ['ADMIN'] },
+    { icon: 'schedule', label: 'Mi Horario', route: '/my-schedule', roles: ['TEACHER'] },
+    { icon: 'check_circle', label: 'Asistencia', route: '/attendance',roles: ['TEACHER'] },
+    { icon: 'account_balance', label: 'Finanzas', route: '/finance', roles: ['ADMIN'] }
   ];
+
+  get visibleNavOptions() {
+    const role = this.userService.getUserRole();
+
+    // Si el rol es válido, filtra según las opciones permitidas
+    if (['ADMIN', 'TEACHER'].includes(role ?? '')) {
+      return this.navOptions.filter(opt => opt.roles.includes(role!));
+    }
+
+    // Si el rol es desconocido o null, mostrar todas las opciones
+    return this.navOptions;
+  }
 
   /**
    * Emits when the logout action is triggered.
    */
   @Output() logout = new EventEmitter<void>();
+
+  handleLogout(): void {
+    this.userService.clearUserData();
+    this.logout.emit();
+    this.router.navigate(['/login']);
+  }
 }
