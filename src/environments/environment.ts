d098ec23002688c36854f87ccbf736eef9b06c70
DanielCr <u20221g120@upc.edu.pe>
2025-07-04 12:41:51 -0500

    feat: update endpoint of environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://6820406072e59f922ef8198b.mockapi.io/api/v1',
+  apiBaseUrl: 'http://localhost:8080/api/v1',
   usersEndpointPath: '/users',
   academyEndpointPath: '/academy',
   academicPeriodEndpointPath: "/academic-periods",
